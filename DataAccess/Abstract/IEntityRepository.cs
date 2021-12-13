47e0f4d35e0b56cdc9a2c904f73febfb2dc62079
mertYlld <mert_1907fb@outlook.com.tr>
2021-12-14 00:48:27 +0300

    C# kamp 8.hafta ders sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity
+    {
+        //generic constraint
+        //class --> referans tip
+        //Ientity --> IEntity olabilir veya Ientity implemente eden bir nesne olabilir
+        //new() --> new'lenebilir olmalı
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
+    }
+}
